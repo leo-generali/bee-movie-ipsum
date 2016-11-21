15335dead81cc4b70aa5abebf122d6753cc832f9
Leo Generali <leogen17@gmail.com>
2016-11-21 00:24:38 -0500

    Initial commit. Base project works.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bee Movie Ipsum Generator
+### About
+Sometimes, you do things that you're not proud of. In this instance, I've made an Ipsum Generator based off of the seminal Jerry Seinfeld classic, Bee Movie. I created this as a way to test my coding abiltiies and to gain experience working within an MVC framework. To that end, I'd say I succeeded.
+
+Live site: leogenerali.com/bee-movie-ipsum
+
+### Still To-Do
+[ ] Make it look nice.
+[ ] Add more lines and quotes.
+[ ] Create single word option.
+[ ] Maybe buy a URL for this. 
+
+### Known Bugs
+[ ] Line breaks are added every new paragraph regardless of whether it's the last one.
+[ ] The same sentnce can show up multiple times in a row.
+
---
+++ js/script.js
@@ -0,0 +1,101 @@
+//Bee Movie Ipsum Generator
+//Created by Leo Generali, started on 11/20
+
+
+//------Model-----//
+
+var model = {
+	sentences: [
+		'According to all known laws of aviation, there is no way a bee should be able to fly.',
+		'You got lint on your fuzz.',
+		'It must be dangerous being a Pollen Jock.',
+		'Ma\'am, I was already a bloodsucking parasite. All I needed was a briefcase!',
+		'Flying is exhausting. Why don\'t you humans just run everywhere, isn\'t that faster?',
+		'I don\'t remember the sun having a big 75 on it.',
+		'Will some angel of mercy come crawl to suck the poison from my heaving buttocks? Fine! Talking bees, No yogurt night... My nerves are fried from riding on this emotional rollarcoaster!',
+		'Watermelon? I thought you said Guatamelan, why would I marry a watermelon?',
+		'Ken, Barry was looking at your résumé and he agreed with me that eating with chopsticks is not really a special skill.',
+		'Why is yogurt night so hard?',
+		'Stand back! These are winter boots!'
+	],
+};
+
+//------Controller-----//
+
+var controller = {
+	//Get sentence length
+	init: function(){
+		this.ipsumObj = {
+			sentences: model.sentences,
+			sentencesLen: model.sentences.length
+		};
+	view.init();
+	},
+
+	randomNumber: function(){
+		return(Math.floor((Math.random() * this.ipsumObj.sentencesLen)));
+	},
+
+	pickQuote: function(){
+		var generatedSingleBeeIpsum = this.ipsumObj.sentences[this.randomNumber()];
+		return(generatedSingleBeeIpsum);
+	},
+
+
+	generateText: function(){
+		this.ipsumMods = view.generatorModifiers();
+		this.generatedIpsum = '';
+		if(this.ipsumMods.type === 'Sentence(s)'){
+			this.generateSentences(this.ipsumMods.count);
+		}else{
+			this.generateParagraphs(this.ipsumMods.count);
+		}
+		view.displayIpsum(this.generatedIpsum);
+	},
+
+	generateSentences: function(sentencesToGenerate){
+		for(var i = 0; i < sentencesToGenerate; i++){
+			this.generatedIpsum += this.pickQuote() + ' ';
+		}
+		this.generatedIpsum += '<br><br>';
+	},
+
+	generateParagraphs: function(paragraphsToGenerate){
+		for(var i = 0; i < paragraphsToGenerate; i++){
+			this.generateSentences(5);
+		}
+	}
+};
+
+//------View-----//
+
+var view = {
+	init: function(){
+		this.resultElem = document.getElementById('bee-result');
+		
+		this.generateButtomElem = document.getElementById('generateTextBtn')
+		this.generateButtomElem.addEventListener('click', this.generateText);
+
+		this.numberModifierElem = document.getElementById('nummberModifier');
+		this.typeModifierElem = document.getElementById('typeModifier');
+	},
+
+	generatorModifiers: function(){
+		this.modifierObj = {
+			count: this.numberModifierElem.value,
+			type: this.typeModifierElem.value
+		};
+		return this.modifierObj;
+	},
+
+	displayIpsum: function(generatedText){
+		this.resultElem.innerHTML = generatedText;
+	},
+
+	generateText: function(){
+		controller.generateText();
+	}
+};
+
+//-----Start the App-----//
+controller.init();
